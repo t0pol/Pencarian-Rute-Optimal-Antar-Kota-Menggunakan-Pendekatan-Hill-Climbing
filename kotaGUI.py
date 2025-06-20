67e941d1cb506a2ff647dc4c70efd038087bfaa3
t0pol <makkamustova65@gmail.com>
2025-06-20 08:28:27 +0700

    kotaGUI.py

--- kotaGUI.py
+++ kotaGUI.py
@@ -10,49 +10,29 @@ root.title("Pencarian Rute Optimal - Hill Climbing")
 
 heuristic_values = {
     'Jakarta': 780, 'Bekasi': 760, 'Bandung': 730, 'Serang': 800,
-    'Karawang': 710, 'Purwakarta': 690, 'Cimahi': 725, 'Garut': 700,
-    'Tasikmalaya': 670, 'Banjar': 650, 'Ciamis': 640, 'Majalengka': 620,
-    'Cirebon': 600, 'Tegal': 580, 'Pekalongan': 550, 'Semarang': 500,
-    'Salatiga': 480, 'Boyolali': 470, 'Solo': 450, 'Sragen': 430,
-    'Ngawi': 400, 'Madiun': 370, 'Nganjuk': 350, 'Kediri': 320,
-    'Blitar': 290, 'Malang': 200, 'Surabaya': 0, 'Sidoarjo': 20,
-    'Cilegon': 790
+    'Karawang': 710, 'Purwakarta': 690, 'Subang': 675, 'Cimahi': 725,
+    'Garut': 700, 'Tasikmalaya': 670, 'Banjar': 650, 'Ciamis': 640,
+    'Majalengka': 620, 'Cirebon': 600, 'Tegal': 580, 'Pekalongan': 550,
+    'Semarang': 500, 'Salatiga': 480, 'Boyolali': 470, 'Solo': 450,
+    'Sragen': 430, 'Ngawi': 400, 'Madiun': 370, 'Nganjuk': 350,
+    'Kediri': 320, 'Blitar': 290, 'Malang': 200, 'Surabaya': 0,
+    'Sidoarjo': 20, 'Cilegon': 790
+}
+
+pos = {
+    'Jakarta': (0.1, 0.5), 'Bekasi': (0.2, 0.5), 'Bandung': (0.3, 0.4),
+    'Serang': (0.05, 0.55), 'Karawang': (0.25, 0.5), 'Purwakarta': (0.3, 0.6),
+    'Subang': (0.32, 0.62), 'Cimahi': (0.35, 0.45), 'Garut': (0.4, 0.4),
+    'Tasikmalaya': (0.45, 0.35), 'Banjar': (0.5, 0.3), 'Ciamis': (0.52, 0.28),
+    'Majalengka': (0.53, 0.33), 'Cirebon': (0.55, 0.35), 'Tegal': (0.6, 0.38),
+    'Pekalongan': (0.65, 0.4), 'Semarang': (0.7, 0.42), 'Salatiga': (0.72, 0.37),
+    'Boyolali': (0.75, 0.35), 'Solo': (0.78, 0.33), 'Sragen': (0.8, 0.3),
+    'Ngawi': (0.83, 0.28), 'Madiun': (0.85, 0.25), 'Nganjuk': (0.87, 0.22),
+    'Kediri': (0.89, 0.2), 'Blitar': (0.91, 0.18), 'Malang': (0.93, 0.15),
+    'Surabaya': (0.97, 0.12), 'Sidoarjo': (0.99, 0.1), 'Cilegon': (0.0, 0.52)
 }
 
 graph = nx.Graph(indonesia_graph)
-pos = {
-    'Jakarta': (0.1, 0.5),
-    'Bekasi': (0.2, 0.5),
-    'Bandung': (0.3, 0.4),
-    'Serang': (0.05, 0.55),
-    'Karawang': (0.25, 0.5),
-    'Purwakarta': (0.3, 0.6),
-    'Subang': (0.32, 0.62),
-    'Cimahi': (0.35, 0.45),
-    'Garut': (0.4, 0.4),
-    'Tasikmalaya': (0.45, 0.35),
-    'Banjar': (0.5, 0.3),
-    'Ciamis': (0.52, 0.28),
-    'Majalengka': (0.53, 0.33),
-    'Cirebon': (0.55, 0.35),
-    'Tegal': (0.6, 0.38),
-    'Pekalongan': (0.65, 0.4),
-    'Semarang': (0.7, 0.42),
-    'Salatiga': (0.72, 0.37),
-    'Boyolali': (0.75, 0.35),
-    'Solo': (0.78, 0.33),
-    'Sragen': (0.8, 0.3),
-    'Ngawi': (0.83, 0.28),
-    'Madiun': (0.85, 0.25),
-    'Nganjuk': (0.87, 0.22),
-    'Kediri': (0.89, 0.2),
-    'Blitar': (0.91, 0.18),
-    'Malang': (0.93, 0.15),
-    'Surabaya': (0.97, 0.12),
-    'Sidoarjo': (0.99, 0.1),
-    'Cilegon': (0.0, 0.52),
-}
-
 
 def find_path():
     start = start_var.get()
